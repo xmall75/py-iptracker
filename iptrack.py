bce28e70509316b099a3acbfb506914bb9025026
Xmall75 ID <34641833+xmall75@users.noreply.github.com>
2021-06-22 10:04:23 +0700

    [Python] IP Tracker

---
+++ iptrack.py
@@ -0,0 +1,16 @@
+import json
+from urllib import request
+
+print("\n== IP Tracker - Xmall75 ==\n")
+url = "http://ip-api.com/json/"
+ip = input("Input the IP Address : ")
+request = request.urlopen(url + ip)
+data_json = json.loads(request.read())
+
+print(f"IP : {data_json['query']}")
+print(f"Country : {data_json['country']}")
+print(f"Region : {data_json['regionName']}")
+print(f"City : {data_json['city']}")
+print(f"Latitude : {data_json['lat']}")
+print(f"Longitude : {data_json['lon']}")
+print(f"ISP : {data_json['isp']}")
